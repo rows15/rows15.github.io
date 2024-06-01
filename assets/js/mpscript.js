9c4f5998cd897eb145123c8940ed86fa8318677c
Rows <marceloxpaduan@gmail.com>
2024-06-01 04:59:40 -0300

    autocommit

--- assets/js/mpscript.js
+++ assets/js/mpscript.js
@@ -1,13 +1,23 @@
 const audio = document.querySelector('audio');
 const coverImage = document.querySelector('.cover img');
 
-// Function to toggle spinning animation
-function toggleSpin() {
-    coverImage.style.animationPlayState = (audio.paused) ? 'paused' : 'running';
+// Function to toggle spinning animation and glowing effect
+function toggleSpinAndGlow() {
+    if (audio.paused) {
+        coverImage.style.animationPlayState = 'paused';
+        coverImage.style.boxShadow = 'none';
+    } else {
+        coverImage.style.animationPlayState = 'running';
+        coverImage.style.boxShadow = 
+            '0 0 10px rgba(255, 255, 255, 0.9), ' + /* inner cyan */
+            '0 0 20px rgba(255, 255, 255, 0.3), ' + /* middle cyan */
+            '0 0 30px rgba(255, 255, 255, 0.3), ' + /* outer white */
+            '0 0 40px rgba(255, 255, 255, 0.3)';    /* outer white */
+    }
 }
 
-audio.addEventListener('play', toggleSpin);
-audio.addEventListener('pause', toggleSpin);
+audio.addEventListener('play', toggleSpinAndGlow);
+audio.addEventListener('pause', toggleSpinAndGlow);
 
 // Update progress bar
 audio.addEventListener('timeupdate', () => {
