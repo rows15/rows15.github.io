365df5da1c5951d5252007fca28ce25dd5f32eae
Rows <marceloxpaduan@gmail.com>
2024-05-31 21:44:29 -0300

    autocommit

--- assets/js/mpscript.js
+++ assets/js/mpscript.js
@@ -1,6 +1,15 @@
 const audio = document.querySelector('audio');
-const progressBar = document.querySelector('.progress');
+const coverImage = document.querySelector('.cover img');
 
+// Function to toggle spinning animation
+function toggleSpin() {
+    coverImage.style.animationPlayState = (audio.paused) ? 'paused' : 'running';
+}
+
+audio.addEventListener('play', toggleSpin);
+audio.addEventListener('pause', toggleSpin);
+
+// Update progress bar
 audio.addEventListener('timeupdate', () => {
     const currentTime = audio.currentTime;
     const duration = audio.duration;
